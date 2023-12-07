2adf1a7bf79a0a6d31ead3fdac3c3e5f6783b5d5
Richard Beaver <rickybeaver3@gmail.com>
2023-12-07 15:53:20 -0500

    Day-03 - completed part 1

---
+++ 2023/rust/day-03/src/bin/part1.rs
@@ -0,0 +1,114 @@
+use std::cmp;
+
+fn main() {
+    let input = include_str!("../../input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+// Sum of all numbers adjacent to any symbol (even diagonally)
+fn part1(input: &str) -> usize {
+    let mut sum = 0;
+    let lines: Vec<_> = input.lines().collect();
+
+    let mut number_locations = Vec::new();
+
+    for (line_num, line) in lines.iter().enumerate() {
+        let mut i = 0;
+        while i < line.len() {
+            let next_num: String = line
+                .get(i..)
+                .unwrap()
+                .chars()
+                .take_while(|c| c.is_numeric())
+                .collect();
+
+            if !next_num.is_empty() {
+                let number =
+                    NumberStringLocation::new(&lines, line_num, i, i + next_num.len()).unwrap();
+                number_locations.push(number);
+            }
+
+            i += cmp::max(next_num.len(), 1);
+        }
+    }
+
+    dbg!(&number_locations);
+
+    for num_loc in number_locations {
+        if any_adjacent_symbol(&lines, num_loc.line, num_loc.idx, num_loc.end_idx) {
+            sum += num_loc.number_str.parse::<usize>().unwrap();
+        }
+    }
+
+    sum
+}
+
+#[derive(Debug)]
+struct NumberStringLocation {
+    number_str: String,
+    line: usize,
+    idx: usize,
+    end_idx: usize,
+}
+
+impl NumberStringLocation {
+    fn new(input_lines: &[&str], line: usize, idx: usize, end_idx: usize) -> Result<Self, ()> {
+        if end_idx <= idx {
+            return Err(());
+        }
+
+        let input_line = *input_lines.get(line).ok_or(())?;
+
+        Ok(Self {
+            number_str: input_line[idx..end_idx].to_owned(),
+            line,
+            idx,
+            end_idx,
+        })
+    }
+}
+
+// Determine if there is a symbol adjacent to the number string described by
+// its line number, its index in its line, and its length
+fn any_adjacent_symbol(input_lines: &[&str], line_num: usize, idx: usize, end_idx: usize) -> bool {
+    let last_line = cmp::max(line_num as isize - 1, 0) as usize;
+    let next_line = cmp::min(line_num + 1, input_lines.len() - 1);
+
+    let min_idx = cmp::max(idx as isize - 1, 0) as usize;
+    let end_idx = cmp::min(end_idx + 1, input_lines.first().unwrap().len());
+
+    for &line in &input_lines[last_line..=next_line] {
+        for char in line[min_idx..end_idx].chars() {
+            if char != '.' && !char.is_numeric() {
+                return true;
+            }
+        }
+    }
+
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let example_input = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
+        let expected_output = 4361;
+
+        let result = part1(example_input);
+
+        assert_eq!(result, expected_output);
+    }
+}
