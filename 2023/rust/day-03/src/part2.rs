5ac7b813cf6af9648cae52e4128de1167bb26133
Richard Beaver <rickybeaver3@gmail.com>
2023-12-07 20:09:14 -0500

    Separate code into lib files

---
+++ 2023/rust/day-03/src/part2.rs
@@ -0,0 +1,18 @@
+pub fn process(_input: &str) -> String {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let example_input = "";
+        let expected_output = "";
+
+        let result = process(example_input);
+
+        assert_eq!(result, expected_output);
+    }
+}
