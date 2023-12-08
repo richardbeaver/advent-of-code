ce1619e57bf8319668dc86831ee64d05db0d4892
Richard Beaver <rickybeaver3@gmail.com>
2023-12-07 20:12:37 -0500

    Update template - separate code into lib files

--- 2023/rust/template/src/bin/part1.rs
+++ 2023/rust/template/src/bin/part1.rs
@@ -1,24 +1,7 @@
+use template::part1::process;
+
 fn main() {
-    let input = include_str!("../../input1.txt");
-    let output = part1(input);
+    let input = include_str!("../../input2.txt");
+    let output = process(input);
     dbg!(output);
-}
-
-fn part1(_input: &str) -> String {
-    todo!()
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let example_input = "";
-        let expected_output = "";
-
-        let result = part1(example_input);
-
-        assert_eq!(result, expected_output);
-    }
 }
