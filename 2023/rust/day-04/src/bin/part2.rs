d76f4234c6fd6847d62e2b1c58b0b6e881e7134d
Richard Beaver <rickybeaver3@gmail.com>
2023-12-15 14:02:10 -0500

    Set up for day 4

---
+++ 2023/rust/day-04/src/bin/part2.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+use day_04::part2::process;
+
+fn main() -> Result<()> {
+    let input = include_str!("../../input2.txt");
+    let output = process(input)?;
+    dbg!(output);
+    Ok(())
+}
---
+++ 2023/rust/day-04/src/part1.rs
@@ -0,0 +1,21 @@
+use anyhow::Result;
+
+pub fn process(_input: &str) -> Result<String> {
+    todo!("part 1")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        todo!("Implement test");
+
+        let example_input = "";
+        let expected_output = "";
+
+        let result = process(example_input).unwrap();
+        assert_eq!(result, expected_output);
+    }
+}
