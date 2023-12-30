b72d422ada1f57460381ce632110d68bb5fbf4e1
Richard Beaver <rickybeaver3@gmail.com>
2023-12-30 18:33:03 -0500

    Set up day 5

---
+++ 2023/rust/day-05/src/bin/part1.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+use day_05::part1::process;
+
+fn main() -> Result<()> {
+    let input = include_str!("../../input1.txt");
+    let output = process(input)?;
+    dbg!(output);
+    Ok(())
+}
