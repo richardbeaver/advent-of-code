db5496339d19516b57bf735f42fe5c798f50984a
Richard Beaver <rickybeaver3@gmail.com>
2025-06-02 19:20:00 -0400

    Move `sys.exit` out of helper function

--- 2024/python/main.py
+++ 2024/python/main.py
@@ -5,12 +5,9 @@ import utils
 
 
 def main(day: int):
-    try:
-        input_text = utils.read_data_input(day)
-        part1 = importlib.import_module(f"day{day:02}.part{1}")
-        part2 = importlib.import_module(f"day{day:02}.part{2}")
-    except (FileNotFoundError, ImportError) as e:
-        sys.exit(f"Error: {e}")
+    input_text = utils.read_data_input(day)
+    part1 = importlib.import_module(f"day{day:02}.part1")
+    part2 = importlib.import_module(f"day{day:02}.part2")
 
     print(f"=== Day {day:02} ===")
     print("Part 1:", part1.part1(input_text))
@@ -22,4 +19,7 @@ if __name__ == "__main__":
     parser.add_argument("day", type=int)
     args = parser.parse_args()
 
-    main(args.day)
+    try:
+        main(args.day)
+    except (FileNotFoundError, ImportError) as e:
+        sys.exit(f"Error: {e}")
